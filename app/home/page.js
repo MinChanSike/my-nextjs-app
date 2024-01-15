57f15825944c01d692e15d6f0650901e73143481
ThuKha <thukha@nete2asia.com>
2024-01-15 15:46:42 +0800

    Test precommit hook

--- app/home/page.js
+++ app/home/page.js
@@ -6,17 +6,17 @@ export default function Home() {
 
   React.useEffect(() => {
     console.log("setInterval");
-    // const interval = setInterval(() => setTime(new Date()), 1000);
-    // return () => {
-    //   console.log("clearInterval");
-    //   clearInterval(interval);
-    // };
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => {
+      console.log("clearInterval");
+      clearInterval(interval);
+    };
   }, []);
 
   return (
     <div>
       <h1>This is under HOME url</h1>
-      {/* {time && <pre>{time.toISOString()}</pre>} */}
+      {Boolean(time) && <pre>{time.toISOString()}</pre>}
     </div>
   );
 }
